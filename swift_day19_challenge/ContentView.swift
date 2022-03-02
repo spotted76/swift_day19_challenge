1a305e1b5fff5bd1e47c428a7197b9b0d7b716a7
Peter Fischer <spotted@gmail.com>
2022-03-01 20:04:32 -0500

    Initial Commit

---
+++ swift_day19_challenge/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swift_day19_challenge
+//
+//  Created by Peter Fischer on 3/1/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swift_day19_challenge/swift_day19_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_day19_challengeApp.swift
+//  swift_day19_challenge
+//
+//  Created by Peter Fischer on 3/1/22.
+//
+
+import SwiftUI
+
+@main
+struct swift_day19_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
